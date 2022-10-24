d5dce4fdcf47e72ee5bb7f22668f885090c804b0
YaninaGazizovna <yanina.mavlyutov@mail.ru>
2022-10-24 10:20:48 +0600

    добавлены react-components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+
+type AppScreenProps = {
+  filmsCount: number;
+  filmRelease:number;
+  filmName:string;
+  filmGenre:string;
+}
+
+function App({filmsCount,filmRelease,filmName,filmGenre}: AppScreenProps): JSX.Element {
+  return (
+    <WelcomeScreen filmsCount={filmsCount} filmRelease={filmRelease} filmName={filmName}
+    filmGenre={filmGenre}/>
+  );
 }
 
 export default App;
